35588521da9ac5633ae32d97e95f7494dee165d9
EugeniuszDe <ewsd1994@gmail.com>
2023-01-23 22:43:24 +0100

    First commit

---
+++ Exersice Materator/Matherator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Provides a variety of numeric methods
+    /// </summary>
+    public class Matherator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Matherator()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the numbers from 1 to 10
+        /// </summary>
+        public void PrintOneToTen()
+        {
+            PrintMToN(1, 10);
+        }
+
+        /// <summary>
+        /// Prints the numbers from m to n
+        /// </summary>
+        /// <param name="m">m</param>
+        /// <param name="n">n</param>
+        public void PrintMToN(int m, int n)
+        {
+            if (m <= n)
+            {
+                for (int j = m; j <= n; j++)
+                {
+                    Console.Write(j + " ");
+
+                }
+            }
+            else
+            {
+                for (int j = m; j >= n; j--)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Returns the tenth even number, with 2 as the first even number
+        /// </summary>
+        /// <returns>tenth even number</returns>
+        public int GetTenthEvenNumber()
+        {
+            // delete code below; only included so we could compile
+            int tenthNum = 2;
+            int count = 1;
+            while (count < 10)
+            {
+                tenthNum++;
+                if (tenthNum % 2 == 0)
+                {
+                    count++;
+                }
+
+            }
+            return tenthNum;
+        }
+    
+
+        /// <summary>
+        /// Returns the nth even number, with 2 as the first even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int GetNthEvenNumber(int n)
+        {
+            // delete code below; only included so we could compile
+            int evenNum = 2;
+            int count = 1;
+            while (count < n)
+            {
+                evenNum++;
+                if (evenNum % 2 == 0)
+                {
+                    count++;
+                }
+
+            }
+            return evenNum;
+
+        }
+
+        #endregion
+    }
+}
---
+++ Programming-Assignment The Digitizer/Digitizer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// Converts words to digits
+    /// </summary>
+    public class Digitizer
+    {
+        #region Fields
+
+        Dictionary<string, int> countDictionary = new Dictionary<string, int>();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Digitizer()
+        {
+            countDictionary.Add("zero", 0);
+            countDictionary.Add("one", 1);
+            countDictionary.Add("two", 2);
+            countDictionary.Add("three", 3);
+            countDictionary.Add("four", 4);
+            countDictionary.Add("five", 5);
+            countDictionary.Add("six", 6);
+            countDictionary.Add("seven", 7);
+            countDictionary.Add("eight", 8);
+            countDictionary.Add("nine", 9);
+
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Converts the given word to the corresponding digit.
+        /// If the word isn't a valid digit name, returns -1
+        /// </summary>
+        /// <param name="word">word to convert</param>
+        /// <returns>corresponding digit or -1</returns>
+        public int ConvertWordToDigit(string word)
+        {
+            string newValue = word.ToLower();
+            if (countDictionary.ContainsKey(newValue))
+            {
+                return countDictionary[newValue];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        #endregion
+    }
+}
